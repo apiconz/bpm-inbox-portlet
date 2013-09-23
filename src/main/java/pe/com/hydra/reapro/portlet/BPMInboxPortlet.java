fccd131a59142df5ab9534378a8ed71307b2da38
Armando <apiconz@gmail.com>
2013-09-23 17:35:22 -0500

    initial

---
+++ src/main/java/pe/com/hydra/reapro/portlet/BPMInboxPortlet.java
@@ -0,0 +1,28 @@
+package pe.com.hydra.reapro.portlet;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class BPMInboxPortlet extends GenericPortlet {
+	
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+		// TODO Auto-generated method stub
+		super.doView(request, response);
+	}
+	
+	@Override
+	public void processAction(ActionRequest request, ActionResponse response)
+			throws PortletException, IOException {
+		// TODO Auto-generated method stub
+		super.processAction(request, response);
+	}
+	
+}

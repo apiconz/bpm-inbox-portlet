d79441b1bd10faee0b544288a974859b4ad2bea3
Armando <apiconz@gmail.com>
2013-09-24 16:33:31 -0500

    Portlet base

--- src/main/java/pe/com/hydra/reapro/portlet/BPMInboxPortlet.java
+++ src/main/java/pe/com/hydra/reapro/portlet/BPMInboxPortlet.java
@@ -10,19 +10,23 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 public class BPMInboxPortlet extends GenericPortlet {
-	
+
 	@Override
 	protected void doView(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
-		// TODO Auto-generated method stub
-		super.doView(request, response);
+
+		response.setContentType("text/html");
+		response.getWriter().println("Hello World");
+
+		getPortletContext().getRequestDispatcher("/WEB-INF/jsp/inbox.jsp").include(request, response);
+		
 	}
-	
+
 	@Override
 	public void processAction(ActionRequest request, ActionResponse response)
 			throws PortletException, IOException {
 		// TODO Auto-generated method stub
 		super.processAction(request, response);
 	}
-	
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/inbox.jsp
@@ -0,0 +1,12 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet"%>
+
+<%@ page import="javax.portlet.*"%>
+<portlet:defineObjects />
+<%
+	//Portlet namespace
+	String portletNS = renderResponse.getNamespace();
+%>
+<h1>Inicial</h1>
+
